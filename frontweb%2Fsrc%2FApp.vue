5f958fe1e77c6317d692b212f3a63651f3ddf2f3
艰苦极度 <105758941+jiankujidu@users.noreply.github.com>
2026-08-25 04:48:02 +0800

    Add frontweb/src/App.vue

---
+++ frontweb%2Fsrc%2FApp.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  overflow: hidden;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+</style>
